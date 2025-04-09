831f42d37433eaca1a71f1b5e41298294e72c4f6
Aayush Parashar <ayushparashar6203@gmail.com>
2025-04-09 17:56:35 +0530

    React lecture 2

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 const parent = React.createElement(
     "div",
     {id: "parent"},
@@ -15,5 +19,5 @@ console.log(parent)
 const heading = React.createElement("h1", { id: "heading" }, "Hello World from React !");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
-//root.render(heading);
+root.render(heading);
+
