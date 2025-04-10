ebff92abaa616962b51cd403656b870c5cbc5ebc
Aayush Parashar <ayushparashar6203@gmail.com>
2025-04-10 19:57:35 +0530

    Episode3

--- App.js
+++ App.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
-const parent = React.createElement(
-    "div",
-    {id: "parent"},
-    React.createElement(
-        "div",
-        {id: "child" },[
-        React.createElement("h1", {}, "I am H1 TAG"),
-        React.createElement("h2", {}, "I am H2 TAG"),
-    ])
+const elem = <span>React Element</span>
+
+
+
+
+//JSX -- is not html in Javascript,  it is a syntax extension to JavaScript
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    {elem}
+    Namaste React using JSX
+  </h1>
 );
 
-console.log(parent)
+//React Component
+//Class Based Component -- Old
+//Functional Component -- New
 
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World from React !");
+//component composition
+const HeadingComponent = () => (
+    <div id="Container">
+    {Title()}
+    
+   <h1 className="Heading">Namaste React Functional Component</h1>
+   </div>
+)
+ 
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-
+root.render(<HeadingComponent/>);
